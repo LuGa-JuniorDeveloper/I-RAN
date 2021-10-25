c508f7791337c048dd84a50bb6481a2242b102e7
Luis Gamarra <luisgamarrarosado@gmail.com>
2021-10-25 02:50:45 -0500

    Desarrollo del formulario de datos de mantenimiento

--- app/src/main/java/com/igp/i_ran/MainActivity.kt
+++ app/src/main/java/com/igp/i_ran/MainActivity.kt
@@ -2,10 +2,16 @@ package com.igp.i_ran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.igp.i_ran.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
